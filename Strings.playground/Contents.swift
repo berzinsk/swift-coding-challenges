9267185c204cd891d0f9d2421d681d73f6122b35
Karlis Berzins <berzinsh.karlis@gmail.com>
2019-04-13 09:06:45 +0300

    challenge 1 completed

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
+// Challenge 1 - Are the letters unique?
+
+func challenge1(input: String) -> Bool {
+    return Set(input).count == input.count
+}
+
+func testChallenge1() {
+    assert(challenge1(input: "No duplicates") == true, "Challenge 1 failed")
+    assert(challenge1(input: "abcdefghijklmnopqrstuvwxyz") == true, "Challenge 1 failed")
+    assert(challenge1(input: "AaBbCc") == true, "Challenge 1 failed")
+    assert(challenge1(input: "Hello world") == false, "Challenge 1 failed")
+
+    print("Challenge 1 sucess")
+}
+
+testChallenge1()
 
