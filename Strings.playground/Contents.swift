9f413f4b4afe815a146d57bc6989d796eb3c89a2
Karlis Berzins <karlis.berzins@accenture.com>
2019-06-18 07:32:35 +0300

    challenge 14 completed

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -368,3 +368,28 @@ func testChallenge13() {
 }
 
 testChallenge13()
+
+
+func challenge14(string: String, current: String = "") {
+    let length = string.count
+    let strArray = Array(string)
+
+    if length == 0 {
+        print(current)
+        print("******")
+    } else {
+        print(current)
+
+        for i in 0..<length {
+            let left = String(strArray[0..<i])
+            let right = String(strArray[i+1..<length])
+            challenge14(string: left + right, current: current + String(strArray[i]))
+        }
+    }
+}
+
+func testChallenge14() {
+    challenge14(string: "abc")
+}
+
+testChallenge14()
